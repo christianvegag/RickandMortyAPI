7aa97926d9a406151ef2d072a6e6161034d0c3c3
christianvegag <christianvegaguizabalo@gmail.com>
2022-02-02 17:03:34 -0500

    RickandMortyAPIv1.1

--- assets/js/api.js
+++ assets/js/api.js
@@ -4,10 +4,10 @@ const getAPI = (url) => {
   return fetch(url)
     .then((response) => response.json())
     .then((json) => {
-      fillData(json.results);
+      fillData(json.results), pagination(json.info);
     })
     .catch((error) => {
-      console.log("Error in the API" , error);
+      console.log("Error in the API", error);
     });
 };
 
@@ -18,13 +18,22 @@ const fillData = (data) => {
     html += '<div class="card h-100">';
     html += `<img src="${ch.image}" class="card-img-top" alt="...">`;
     html += '<div class="card-body">';
-    html += `<h5 class="card-title">${ch.name}</h5>`
+    html += `<h5 class="card-title">${ch.name}</h5>`;
     html += "</div>";
     html += "</div>";
     html += "</div>";
   });
 
   document.getElementById("characters").innerHTML = html;
+};
+
+const pagination = (info) => {
+  let html = "";
+
+  html += `<li style="cursor:pointer;" class="page-item ${info.prev == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.prev}')">Prev</a></li>`;
+  html += `<li style="cursor:pointer;" class="page-item ${info.next == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.next}')">Next</a></li>`;
+
+  document.getElementById("pagination").innerHTML = html;
 };
 
 getAPI(API);

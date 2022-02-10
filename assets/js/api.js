120e680782ac39a8e4de9c5f0362ee1124703410
christianvegag <christianvegaguizabalo@gmail.com>
2022-02-10 00:36:48 -0500

    Actualizacion de Header y estilos

--- assets/js/api.js
+++ assets/js/api.js
@@ -14,14 +14,25 @@ const getAPI = (url) => {
 const fillData = (data) => {
   let html = "";
   data.forEach((ch) => {
-    html += '<div class="col">';
-    html += '<div class="card h-100">';
-    html += `<img src="${ch.image}" class="card-img-top" alt="...">`;
-    html += '<div class="card-body">';
-    html += `<h5 class="card-title">${ch.name}</h5>`;
-    html += "</div>";
-    html += "</div>";
-    html += "</div>";
+    html += `<div class="card" >
+    <div class="image">
+      <img
+        src="${ch.image}" />
+    </div>
+    <div class="details">
+      <div class="center">
+        <h1>${ch.name}</h1>
+        <p class="text-center text-white"><span class="${
+          ch.status == "Alive" ? "dot" : "dotDead"
+        }"></span>&nbsp;${ch.status} - ${ch.species}</p>
+        <br>
+        <p><span class="text-white-50">Gender : </span>${ch.gender} </p>
+        <p><span class="text-white-50">Location : </span> ${
+          ch.location.name
+        }</p>
+      </div>
+    </div>
+  </div>`;
   });
 
   document.getElementById("characters").innerHTML = html;
@@ -30,8 +41,16 @@ const fillData = (data) => {
 const pagination = (info) => {
   let html = "";
 
-  html += `<li style="cursor:pointer;" class="page-item ${info.prev == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.prev}')">Prev</a></li>`;
-  html += `<li style="cursor:pointer;" class="page-item ${info.next == null ? "disabled" : ""}"><a class="page-link" onclick="getAPI('${info.next}')">Next</a></li>`;
+  html += `<li style="cursor:pointer;" class="page-item ${
+    info.prev == null ? "disabled" : ""
+  }"><a class="page-link bg-transparent" onclick="getAPI('${
+    info.prev
+  }')">Prev</a></li>`;
+  html += `<li style="cursor:pointer;" class="page-item ${
+    info.next == null ? "disabled" : ""
+  }"><a class="page-link bg-transparent" onclick="getAPI('${
+    info.next
+  }')">Next</a></li>`;
 
   document.getElementById("pagination").innerHTML = html;
 };
